401075db1f336413ce5fba4400de689d990a780a
Mhmdzein1 <mohammad_zein_2000@hotmail.com>
2024-05-06 18:27:41 +0300

    edit js code

---
+++ calculator/newScript.js
@@ -0,0 +1,47 @@
+const buttonIds = [
+                  "+", "7", "4", "1", "0", "-", "8", "5", "2", 
+                  "x", "9", "6", "3", ".", "c", "equal", "/"
+                   ];
+  
+  buttonIds.forEach(function(id) {
+    const button = document.getElementById(id);
+    button.addEventListener("click", function() {
+      handleButtonClick(id);
+    });
+  });
+  
+  function handleButtonClick(value) {
+    
+    if (value === "c") {
+      ans.innerHTML = " ";
+    }
+    else if (value === "/") {
+        if(    ans.innerHTML===" "){
+            return;
+        }
+        ans.innerHTML+="/"
+      }
+      else if (value === "+") {
+        if(    ans.innerHTML===" "){
+            return;
+        }
+        ans.innerHTML+="+"
+      }
+      else if (value === "-") {
+        if(    ans.innerHTML===" "){
+            return;
+        }
+        ans.innerHTML+="-"
+      }
+      else if (value === "x") {
+        if(    ans.innerHTML===" "){
+            return;
+        }
+        ans.innerHTML+="*"
+      }
+     else if (value === "equal") {
+      ans.innerHTML = eval(ans.innerHTML);
+    } else {
+      ans.innerHTML += value;
+    }
+  }
